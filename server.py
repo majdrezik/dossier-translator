c8b143445f982b9d2963a19442917c09cef0707f
Majd Rezik <majdrezik@gmail.com>
2022-11-22 17:26:09 +0200

    added login and signupUser and signupTester

--- server.py
+++ server.py
@@ -18,5 +18,20 @@ def login():
     return render_template('login_page.html')
 
 
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+
+@app.route("/signupUser")
+def signup_user():
+    return render_template('signup_user.html')
+
+
+@app.route("/signupTester")
+def signup_tester():
+    return render_template('signup_tester.html')
+
+
 if __name__ == "__main__":
     app.run()
