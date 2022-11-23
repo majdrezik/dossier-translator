39f8a6359f382118af452c8bf907ccd53696c8df
Majd Rezik <majdrezik@gmail.com>
2022-11-23 11:24:55 +0200

    refactored login and signupUser and signupTester

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # dossier-translator
+
 software engineering degree final project
+
+1. **clone the project via ssh:**
+
+#
+
+    git clone git@github.com:majdrezik/dossier-translator.git
+
+#
+
+2. **install the required packages:**
+
+#
+
+    cd dossier-translator
+    pip3 install -r requirements.txt
+
+#
+
+3. **run the server:**
+
+#
+
+    FLASK_APP=server.py flask run
+
+Navigate to `http://127.0.0.1:5000/login` in your browser, ENJOY!
--- server.py
+++ server.py
@@ -1,21 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, g
 import os
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
 
 
 template_dir = os.path.abspath('templates/')
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__, template_folder=template_dir)
+# app.config['DB_USERNAME'] = environ.get('DB_USERNAME')
+# app.config['DB_PASSWORD'] = environ.get('DB_PASSWORD')
+os.environ['DB_USERNAME'] = "majd"
+os.environ['DB_PASSWORD'] = "majd"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = "mongodb://" + os.getenv('DB_USERNAME') + ":" + os.getenv(
+    'DB_USERNAME') + "@ac-sq9li2w-shard-00-00.xexprlu.mongodb.net:27017,ac-sq9li2w-shard-00-01.xexprlu.mongodb.net: 27017,ac-sq9li2w-shard-00-02.xexprlu.mongodb.net:27017/?ssl=true&replicaSet=atlas-9z9y2w-shard-0&authSource=admin&retryWrites=true&w=majority"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 # FLASK_APP=server.py flask run
 
 
+# Use LocalProxy to read the global db instance with just `db`
+# def get_db():
+#     """
+#     Configuration method to return db instance
+#     """
+#     db = getattr(g, "_database", None)
+#     if db is None:
+#         db = g._database = PyMongo(current_app).db
+#     return db
+# db = LocalProxy(get_db)
+
+
 @app.route("/")
-def hello_world():
-    return "hello world!"
-
-
-@app.route("/login")
 def login():
     return render_template('login_page.html')
+
+
+@app.route("/login", methods=['POST'])
+def auth():
+    req = request.get_json()
+    # This is the query that was stored in the JSON within the browser
+    print('query : ', req)
+    print(type(req))
+    print('------------------------')
+    # this converts the json query to a python dictionary
+    reqToProcess = json.loads(req)
+    print(reqToProcess)
+    print('username: ', reqToProcess['username'])
+    print('password: ', reqToProcess['password'])
+    resp = jsonify(success=True)
+    return resp
 
 
 @app.route("/signup")
@@ -31,6 +71,34 @@ def signup_user():
 @app.route("/signupTester")
 def signup_tester():
     return render_template('signup_tester.html')
+
+
+@app.route("/user_homepage")
+def user_homepage():
+    return "hello user"
+
+
+@app.route("/tester_homepage")
+def tester_homepage():
+    return "hello tester"
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'hi'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
 
 
 if __name__ == "__main__":
