9fba19f69b8a5ebfc97f22a5d292bfa7aed65238
Majd Rezik <majdrezik@gmail.com>
2023-01-06 06:44:06 +0200

    created db and now login works fine for USER

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ software engineering degree final project
 
 1. **clone the project via ssh:**
 
+works: docker image build -t doss_app .
+works: docker run --name doss_app_container -p 8080:8080 -d doss_app
+
 #
 
     git clone git@github.com:majdrezik/dossier-translator.git
@@ -15,7 +18,10 @@ software engineering degree final project
 #
 
     cd dossier-translator
-    pip3 install -r requirements.txt
+
+ONLY ONCE:
+
+    docker build . -t doss_app
 
 #
 
@@ -23,6 +29,11 @@ software engineering degree final project
 
 #
 
-    FLASK_APP=server.py flask run
+WHEN THE IMAGE IS DONE, RUN:
+
+    docker run -it --name doss_app_container \
+    -v $PWD:/dossier-translator \
+    -p 5000:5000 \
+    doss_app
 
 Navigate to `http://127.0.0.1:5000/login` in your browser, ENJOY!
--- server.py
+++ server.py
@@ -1,53 +1,223 @@
-from flask import Flask, render_template, request, jsonify, redirect, g
+from flask import Flask, render_template, request, jsonify
 import os
 from os import environ
-from flask_sqlalchemy import SQLAlchemy
 import json
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
-import fitz
-from deep_translator import GoogleTranslator
-from fpdf import FPDF
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
+import PyPDF2
+# from deep_translator import GoogleTranslator
+# from fpdf import FPDF
+# from reportlab.pdfgen import canvas
+# from reportlab.lib.pagesizes import letter
+# import sqlite3
+from sqlite3 import Error
+import mysql.connector
+from jinja2 import Environment, FileSystemLoader
+
+
+# con = sqlite3.connect("dossier.db")
+# conn = None
+does_testers_table_exist = False
+does_users_table_exist = False
 
 template_dir = os.path.abspath('templates/')
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, template_folder=template_dir)
-# app.config['DB_USERNAME'] = environ.get('DB_USERNAME')
-# app.config['DB_PASSWORD'] = environ.get('DB_PASSWORD')
-os.environ['DB_USERNAME'] = "majd"
-os.environ['DB_PASSWORD'] = "majd"
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = "mongodb://" + os.getenv('DB_USERNAME') + ":" + os.getenv(
-    'DB_USERNAME') + "@ac-sq9li2w-shard-00-00.xexprlu.mongodb.net:27017,ac-sq9li2w-shard-00-01.xexprlu.mongodb.net: 27017,ac-sq9li2w-shard-00-02.xexprlu.mongodb.net:27017/?ssl=true&replicaSet=atlas-9z9y2w-shard-0&authSource=admin&retryWrites=true&w=majority"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 
 # FLASK_APP=server.py flask run
 
+'''
 
-# Use LocalProxy to read the global db instance with just `db`
-# def get_db():
-#     """
-#     Configuration method to return db instance
-#     """
-#     db = getattr(g, "_database", None)
-#     if db is None:
-#         db = g._database = PyMongo(current_app).db
-#     return db
-# db = LocalProxy(get_db)
+docker run -it --name doss_app_container\
+-v $PWD:/dossier-translator \
+-p 5000:5000 \
+doss_app
+
+'''
 
 
-@app.route("/")
+environment = Environment(loader=FileSystemLoader("templates/"))
+
+# delete
+
+
+# @app.route("/tester_homepage")
+# def test_jinja():
+#     return render_template("waiting_documents.html", tester=json.loads('{"majd": "rezik"}'))
+
+
+class User:
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+
+def create_connection():
+    try:
+        conn = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            # password="root",
+            # database="doss_app",
+            port="3306"
+        )
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            create_table_sql
+        )
+    except Error as e:
+        print(e)
+
+
+@app.route("/",  methods=['GET', 'POST'])
 def login():
+    # create_connection()
     return render_template('login_page.html')
 
 
-@app.route("/login", methods=['POST'])
+@app.route("/auth0", methods=['GET', 'POST'])
 def auth():
+    if request.method == 'POST':
+        print("post")
+        req = request.get_json()
+        # This is the query that was stored in the JSON within the browser
+        print('query : ', req)
+        # print(type(req))
+        print('------------------------')
+        # this converts the json query to a python dictionary
+        print('1')
+        # reqToProcess = json.loads(req)
+        reqToProcess = req
+
+        print(reqToProcess)
+        print(type(reqToProcess))
+        username = reqToProcess['username']
+        print("username:")
+        print(reqToProcess['username'])
+        password = reqToProcess['password']
+        print("password:")
+        print(reqToProcess['password'])
+        # resp = jsonify(success=True)
+
+        table = "testers" if (
+            (get_user_position(username)).__eq__('tester')
+        ) else "users"
+        print("table:" + table)
+        if (are_credentials_valid(table, username, password) == True):
+            if (table.__eq__("testers")):
+                # return render_template('waiting_documents.html', tester=json.loads(get_tester_by_name_as_json(username)))
+                return ["user_homepage", json.loads(get_tester_by_name_as_json(username))]
+
+            else:  # user
+                current_user = json.loads(get_user_by_name_as_json(username))
+                print("current user: ")
+                print(current_user)
+                print(type(current_user))
+                return jsonify(
+                    # status=200,
+                    result=current_user,
+                    redirect_path="user_homepage"
+                )
+    else:
+        return "GET"
+
+
+def get_user_by_name_as_json(username):
+    cursor = conn.cursor(dictionary=True)  # to return a dictionary
+    user_query = "SELECT * FROM doss_sc.users where username=%s"
+    result_user = cursor.execute(user_query, (username,))
+    user = cursor.fetchone()
+    if (user == None):
+        print("no user with username: " + username)
+    print(f"user as JSON: {json.dumps(user)} ")
+    return json.dumps(user)  # converts return value to JSON
+
+
+def get_tester_by_name_as_json(username):
+    cursor = conn.cursor()  # to return a dictionary
+    tester_query = "SELECT * FROM doss_sc.testers where username=%s"
+    result_user = cursor.execute(tester_query, (username,))
+    tester = cursor.fetchone()
+    if (tester == None):
+        print("no tester with username: " + username)
+    print(f"tester as JSON: {json.dumps(tester)} ")
+    return json.dumps(tester)  # converts return value to JSON
+
+
+def get_user_position(username):
+    print("called get_user_position for " + username)
+    position = 'user'
+    cursor = conn.cursor()
+    sql_user = "SELECT * FROM doss_sc.users where username=%s"
+    result_user = cursor.execute(sql_user, (username,))
+    row = cursor.fetchone()
+    if (row == None):
+        sql_tester = "SELECT * FROM doss_sc.testers where username=%s"
+        result_tester = cursor.execute(sql_tester, (username,))
+        row = cursor.fetchone()
+        if (row == None):
+            position = None
+        else:
+            print('tester')
+            position = 'tester'
+    print('user')
+    return position
+
+
+def are_credentials_valid(table, username, password):
+    print("called are_credentials_valid")
+    cursor = conn.cursor()
+    sql = "SELECT * FROM doss_sc." + table + " where username=%s and password=%s"
+    print(sql)
+    result = cursor.execute(sql, (username, password,))
+    row = cursor.fetchone()
+    return False if row == None else True
+
+
+@ app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+
+@ app.route("/signupUser")
+def signup_user():
+    return render_template('signup_user.html')
+
+
+@ app.route("/attempt_signup_user", methods=['POST'])
+def attempt_signup_user():
+    is_users_table_created = create_users_table_if_doesnt_exist()
+    req = request.get_json()
+    print("GOT here to attempt_signup_user.server on route /attempt_signup_user")
+    print('query : ', req)
+    print(type(req))
+    reqToProcess = json.loads(req)
+    print(reqToProcess)
+    username = reqToProcess['username']
+    print('username: ', reqToProcess['username'])
+    password = reqToProcess['password']
+    print("##################")
+    print("Finished attempt_signup_user.SERVER")
+    # register_user()
+    resp = jsonify({"name": "majd-test"})
+    return resp
+
+
+def register_user():
     req = request.get_json()
     # This is the query that was stored in the JSON within the browser
     print('query : ', req)
@@ -56,73 +226,116 @@ def auth():
     # this converts the json query to a python dictionary
     reqToProcess = json.loads(req)
     print(reqToProcess)
+    username = reqToProcess['username']
     print('username: ', reqToProcess['username'])
+    password = reqToProcess['password']
     print('password: ', reqToProcess['password'])
     resp = jsonify(success=True)
-    return resp
+    if (are_credentials_valid(username, password)):
+        return True
 
 
-@app.route("/signup")
-def signup():
-    return render_template('signup.html')
-
-
-@app.route("/signupUser")
-def signup_user():
-    return render_template('signup_user.html')
-
-
-@app.route("/signupTester")
+@ app.route("/signupTester")
 def signup_tester():
+    is_testers_table_created = create_testers_table_if_doesnt_exist()
+
+
+# def create_testers_table_if_doesnt_exist():
+#     sql_create_testers_table = """ CREATE TABLE IF NOT EXISTS testers (
+#                                             id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+#                                             name text NOT NULL,
+#                                             email text,
+#                                             age integer,
+#                                             password text,
+#                                             type text,
+#                                             languages text,
+#                                             files_path text
+#                                         ); """
+#     cursor = conn.cursor()
+#     if does_testers_table_exist is False:
+#         if conn is not None:
+#             # create projects table
+#             create_table(conn, sql_create_testers_table)
+#             does_testers_table_exist = True
+#         else:
+#             print("Error! cannot create the database connection.")
+#     return does_testers_table_exist
+
+
+def create_users_table_if_doesnt_exist():
+    print("checking users table...")
+    sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
+                                            id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                                            name text NOT NULL,
+                                            email text,
+                                            age integer,
+                                            password text,
+                                            type text,
+                                            files_path text
+                                        ); """
+    if does_users_table_exist is False:
+        if conn is not None:
+            # create projects table
+            print("creating users table...")
+            create_table(conn, sql_create_users_table)
+            does_users_table_exist = True
+        else:
+            print("Error! cannot create the database connection.")
+    return does_users_table_exist
+
+
+@ app.route("/attempt_signup_tester")
+def attempt_signup_tester():
     return render_template('signup_tester.html')
 
 
-@app.route("/user_homepage")
-def user_homepage():
+@ app.route("/user_homepage",  methods=['GET', 'POST'])
+def user_homepage_controller():
     return render_template('upload_file.html')
 
 
-@app.route("/tester_homepage")
+@ app.route("/tester_homepage")
 def tester_homepage():
     return render_template('waiting_documents.html')
 
-@app.route("/archivepage")
+
+@ app.route("/archivepage")
 def archivepage():
     return render_template('Archive.html')
 
 
-@app.route("/exportpage")
+@ app.route("/exportpage")
 def exportpage():
     return render_template('Export_page.html')
 
-@app.route("/testerchecktranslation")
+
+@ app.route("/testerchecktranslation")
 def checkingpage():
     return render_template('tester_check_translation.html')
 
 
-
-@app.route('/test', methods=['GET'])
+@ app.route('/test', methods=['GET'])
 def test():
     langs_dict = GoogleTranslator.get_supported_languages('self')
     return 'hi'
 
 
-@app.route("/send_translation_to_tester")
+@ app.route("/send_translation_to_tester")
 def send_to_tester():
     return render_template('tester_check_translation.html')
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    age = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     firstname = db.Column(db.String(100), nullable=False)
+#     lastname = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(80), unique=True, nullable=False)
+#     age = db.Column(db.Integer)
+#     created_at = db.Column(db.DateTime(timezone=True),
+#                            server_default=func.now())
 
-    def __repr__(self):
-        return f'<Student {self.firstname}>'
+#     def __repr__(self):
+#         return f'<Student {self.firstname}>'
 
 
 # def read_file(input_file):
@@ -170,7 +383,7 @@ def write_translation_to_txt_file(input_file, path):
     # pdf.output("test/output.pdf")
 
 
-@app.route("/translate_file")
+@ app.route("/translate_file")
 def translate_file():
     # read_file(input_file)
     input_file = read_file()
@@ -190,5 +403,22 @@ def translate_file():
     return 'ok'
 
 
+def main():
+    print("###########################")
+    print("###########################")
+    print('start main')
+    print("###########################")
+    print("###########################")
+    port = int(os.environ.get('PORT', 8080))
+    # main()
+    global conn
+    print('creating connection')
+    conn = create_connection()
+    print('connection established')
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
+
+main()
